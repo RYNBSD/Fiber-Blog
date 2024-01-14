290b75a5d9aeb663c7ef06d8ba2257ea9ea7cd49
RYN BSD <rynbsd04@gmail.com>
2024-01-14 10:14:11 +0100

    config me endpoint response

--- controller/auth.go
+++ controller/auth.go
@@ -10,51 +10,37 @@ import (
 func SignUp(c *fiber.Ctx) error {
 	var body *schema.SignUp
 	if err := c.BodyParser(&body); err != nil {
-
+		return err
 	}
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": true,
-	})
+	return c.SendStatus(fiber.StatusCreated)
 }
 
 func SignIn(c *fiber.Ctx) error {
 	var body *schema.SignIn
 	if err := c.BodyParser(&body); err != nil {
-
+		return err
 	}
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": true,
-	}) 
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func SignOut(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": true,
-	})
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func Me(c *fiber.Ctx) error {
-	authorization := c.Get("authorization", "")
+	authorization := c.Get(fiber.HeaderAuthorization, "")
 	if len(authorization) == 0 {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"success": false,
-			"message": "Empty authorization key",
-		})
+		return fiber.ErrUnauthorized
 	}
 
 	_, err := util.VerifyJwt(authorization)
 	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"success": false,
-			"message": err.Error(),
-		})
+		return fiber.ErrUnauthorized
 	}
 
 	// TODO: add function to verify if user exists
 
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-		"success": true,
-	})
+	return c.SendStatus(fiber.StatusCreated)
 }
