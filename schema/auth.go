d0ca353cb98293d0110a0eeff6f0a564803041f9
RYN BSD <rynbsd04@gmail.com>
2024-01-14 10:20:14 +0100

    set auth schema tags

--- schema/auth.go
+++ schema/auth.go
@@ -3,9 +3,12 @@ package schema
 // Request Body Schema For All Auth Endpoints
 
 type SignUp struct {
-
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type SignIn struct {
-
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
