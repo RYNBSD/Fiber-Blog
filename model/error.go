3f7882ce12e513af752c9ddd0d169ba6ee71538c
RYN BSD <rynbsd04@gmail.com>
2024-02-04 05:43:27 +0100

    fix error queries

--- model/error.go
+++ model/error.go
@@ -2,7 +2,7 @@ package model
 
 func (e *Error) CreateError() {
 	Connect()
-	sql := `INSERT INTO error(status, message) VALUES (?, ?)`
+	sql := `INSERT INTO "error"("status", "message") VALUES ($1, $2)`
 
 	if _, err := DB.Exec(sql, e.Status, e.Message); err != nil {
 		panic(err)
